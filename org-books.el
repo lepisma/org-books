45f0e6e698c44ea8eec060753d9f43e160cc8138
Abhinav Tushar <abhinav@lepisma.xyz>
2020-02-01 15:56:43 +0530

    0.2.14

--- org-books.el
+++ org-books.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2017 Abhinav Tushar
 
 ;; Author: Abhinav Tushar <abhinav.tushar.vs@gmail.com>
-;; Version: 0.2.13
+;; Version: 0.2.14
 ;; Package-Requires: ((enlive "0.0.1") (s "1.11.0") (helm "2.9.2") (helm-org "1.0") (dash "2.14.1") (emacs "25"))
 ;; URL: https://github.com/lepisma/org-books
 ;; Keywords: outlines
