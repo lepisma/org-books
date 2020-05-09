7ea20679f8f8b9ba53d976ec83f16278215ee89b
Abhinav Tushar <abhinav@lepisma.xyz>
2020-05-09 21:27:12 +0530

    merge get-details file in org-books itself

--- org-books.el
+++ org-books.el
@@ -30,13 +30,17 @@
 
 ;;; Code:
 
-(require 'org)
-(require 's)
+(require 'cl-lib)
+(require 'dash)
+(require 'enlive)
+(require 'json)
 (require 'helm)
 (require 'helm-org)
-(require 'dash)
-(require 'org-books-get-details)
-
+(require 'org)
+(require 's)
+(require 'subr-x)
+(require 'url)
+(require 'url-parse)
 
 (defgroup org-books nil
   "Org reading list management."
@@ -56,6 +60,75 @@
   "The max depth for adding book under headings."
   :type 'integer
   :group 'org-books)
+
+(defcustom org-books-url-patterns
+  '((amazon . "^\\(www\\.\\)?amazon\\.")
+    (goodreads . "^\\(www\\.\\)?goodreads\\.com")
+    (isbn . "openlibrary\\.org"))
+  "Patterns for detecting url types."
+  :group 'org-books)
+
+(defun org-books--get-json (url)
+  (with-current-buffer (url-retrieve-synchronously url)
+    (goto-char (point-min))
+    (re-search-forward "^$")
+    (json-read)))
+
+(defun org-books--clean-str (text)
+  (s-trim (s-collapse-whitespace text)))
+
+(defun org-books-get-url-type (url pattern-alist)
+  "Return type of url using the regex pattern."
+  (unless (null pattern-alist)
+    (let ((pattern (cdr (car pattern-alist))))
+      (if (s-matches? pattern (url-host (url-generic-parse-url url)))
+          (caar pattern-alist)
+        (org-books-get-url-type url (cdr pattern-alist))))))
+
+(defun org-books-get-details (url url-type)
+  (cl-case url-type
+    (amazon (org-books-get-details-amazon url))
+    (goodreads (org-books-get-details-goodreads url))
+    (isbn (org-books-get-details-isbn url))))
+
+(defun org-books-get-details-amazon-authors (page-node)
+  "Return author names for amazon page."
+  (or (mapcar #'enlive-text (enlive-query-all page-node [.a-section .author .contributorNameID]))
+      (mapcar #'enlive-text (enlive-query-all page-node [.a-section .author > a]))))
+
+(defun org-books-get-details-amazon (url)
+  "Get book details from amazon page."
+  (let* ((page-node (enlive-fetch url))
+         (title (org-books--clean-str (enlive-text (enlive-get-element-by-id page-node "productTitle"))))
+         (author (s-join ", " (org-books-get-details-amazon-authors page-node))))
+    (if (not (string-equal title ""))
+        (list title author `(("AMAZON" . ,url))))))
+
+(defun org-books-get-details-goodreads (url)
+  "Get book details from goodreads page."
+  (let* ((page-node (enlive-fetch url))
+         (title (org-books--clean-str (enlive-text (enlive-get-element-by-id page-node "bookTitle"))))
+         (author (org-books--clean-str (s-join ", " (mapcar #'enlive-text (enlive-query-all page-node [.authorName > span]))))))
+    (if (not (string-equal title ""))
+        (list title author `(("GOODREADS" . ,url))))))
+
+(defun org-books-get-url-from-isbn (isbn)
+  (concat "https://openlibrary.org/api/books?bibkeys=ISBN:" isbn "&jscmd=data&format=json"))
+
+(defun org-books-get-details-isbn (url)
+  "Get book details from openlibrary ISBN response."
+  (let* ((json-object-type 'hash-table)
+         (json-array-type 'list)
+         (json-key-type 'string)
+         (json (org-books--get-json url))
+         (isbn (car (hash-table-keys json)))
+         (data (gethash isbn json))
+         (title (gethash "title" data))
+         (authors (gethash "authors" data))
+         (author (gethash "name" (car authors))))
+    (list title author `(("ISBN" . ,url)))))
+
+;; -----
 
 (defun org-books-create-file (file-path)
   "Write initialization stuff in a new file at FILE-PATH."
